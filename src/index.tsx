224562cea8e7ec287a373ca78791806f8690586a
Chloe Mae McMullan <pacchhhiiii@googlemail.com>
2021-03-07 15:53:51 +0000

    All strategies. Missing some test cases.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Input from './Input';
+import { TwoCommaDelimitedSumStrategy } from './validation/strategy/TwoCommaDelimitedSumStrategy';
+import { CommaDelimitedNumberSumStrategy } from './validation/strategy/CommaDelimitedNumberSumStrategy';
+import { CommaDelimitedCappedSumStrategy } from './validation/strategy/CommaDelimitedCappedSumStrategy';
+import { PositiveCommaDelimitedNumberSumStrategy } from './validation/strategy/PositiveCommaDelimitedNumberSumStrategy';
+import { CustomDelimitedSumStrategy } from './validation/strategy/CustomDelimitedSumStrategy';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>Type 1: Two comma delimited numbers</h1>
+    <Input strategy={new TwoCommaDelimitedSumStrategy()} />
+    <h1>Type 2: X comma delimited numbers</h1>
+    <Input strategy={new CommaDelimitedNumberSumStrategy()} />
+    <h1>Type 3: Negative numbers ignored</h1>
+    <Input strategy={new PositiveCommaDelimitedNumberSumStrategy()} />
+    <h1>Type 4: Comma delimited capped numbers (capped at 1001)</h1>
+    <Input strategy={new CommaDelimitedCappedSumStrategy(1001)} />
+    <h1>Type 5: Custom delimiter in format //[delimiter]\n[values]</h1>
+    <Input strategy={new CustomDelimitedSumStrategy(1001)} />
   </React.StrictMode>,
   document.getElementById('root')
 );
